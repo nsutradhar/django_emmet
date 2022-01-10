94d4d75d230736b0d9f9a17eaec422483f587507
narattam1996 <narattam555@gmail.com>
2022-01-10 20:52:06 +0530

    initial

---
+++ root/README.md
@@ -0,0 +1,19 @@
+# django-boilerplate
+
+## Django boilerplate
+
+### instruction
+
+### if you want to rename the project then Following for this
+
+### python3 manage.py rename "old_name", then "new_name"
+### virtualenv env command in the root folder
+### then active it 
+
+### pip3 install -r requirment.txt
+
+### then run python3 manage.py collectstatic
+
+### then finally run server
+### already have a admin
+### admin name alix and password alix
---
+++ root/project/settings/production.py
@@ -0,0 +1,35 @@
+from .base import *
+# from decouple import config
+
+DEBUG = config('DEBUG', cast=bool)
+ALLOWED_HOSTS = ['ip-address', 'wwww.website-name']
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASS'),
+        'HOST': config('DB_HOST'),
+        'PORT': ''
+    }
+}
+
+STRIPE_PUBLIC_KEY = config('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
